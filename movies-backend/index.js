1a3d1c14c33bdafa3511d1b4518c9e97651b74c8
serhat-bca <sersen@bergen.org>
2025-01-16 14:18:37 -0500

    config used for port.

--- movies-backend/index.js
+++ movies-backend/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-require("dotenv").config();
-const port = process.env.PORT;
+const config = require("./utils/config");
+const port = config.port;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
