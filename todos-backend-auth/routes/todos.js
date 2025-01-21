4caa82a696283a113bf9ca1c84e279abb7023721
serhat-bca <sersen@bergen.org>
2025-01-21 15:27:18 -0500

    try catch eliminated with async-errors library

--- todos-backend-auth/routes/todos.js
+++ todos-backend-auth/routes/todos.js
@@ -2,18 +2,14 @@ const express = require("express");
 const todosRouter = express.Router();
 const Todo = require("../models/todo");
 
-todosRouter.post("/", async (req, res, next) => {
-  try {
-    const { task, done } = req.body;
-    if (!task) {
-      return res.status(400).json({ error: "Task is required" });
-    } else {
-      const todo = new Todo({ task, done: done || false });
-      const savedTodo = await todo.save();
-      res.json(savedTodo);
-    }
-  } catch (error) {
-    next(error);
+todosRouter.post("/", async (req, res) => {
+  const { task, done } = req.body;
+  if (!task) {
+    return res.status(400).json({ error: "Task is required" });
+  } else {
+    const todo = new Todo({ task, done: done || false });
+    const savedTodo = await todo.save();
+    res.json(savedTodo);
   }
 });
 
@@ -31,36 +27,28 @@ todosRouter.get("/:id", async (req, res) => {
   }
 });
 
-todosRouter.delete("/:id", async (req, res, next) => {
-  try {
-    const todo = await Todo.findByIdAndDelete(req.params.id);
-    if (!todo) {
-      res.status(404).json({ error: "Task not found!" });
-    } else {
-      res
-        .status(200)
-        .json({ message: `The task [${todo.task}] deleted successfully.` });
-    }
-  } catch (error) {
-    next(error);
+todosRouter.delete("/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndDelete(req.params.id);
+  if (!todo) {
+    res.status(404).json({ error: "Task not found!" });
+  } else {
+    res
+      .status(200)
+      .json({ message: `The task [${todo.task}] deleted successfully.` });
   }
 });
 
-todosRouter.put("/:id", async (req, res, next) => {
-  try {
-    const { task, done } = req.body;
-    const updatedTodo = await Todo.findByIdAndUpdate(
-      req.params.id,
-      { task, done },
-      { new: true, runValidators: true }
-    );
-    if (updatedTodo) {
-      res.status(200).json(updatedTodo);
-    } else {
-      res.status(404).json({ error: "Task not found" });
-    }
-  } catch (error) {
-    next(error);
+todosRouter.put("/:id", async (req, res) => {
+  const { task, done } = req.body;
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    req.params.id,
+    { task, done },
+    { new: true, runValidators: true }
+  );
+  if (updatedTodo) {
+    res.status(200).json(updatedTodo);
+  } else {
+    res.status(404).json({ error: "Task not found" });
   }
 });
 
