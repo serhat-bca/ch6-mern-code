e7c7af9858a7d1fa7a52b2613e3b97eb27495969
serhat-bca <sersen@bergen.org>
2025-01-24 15:46:48 -0500

    this is for yash the one.

--- todos-backend-auth/routes/users.js
+++ todos-backend-auth/routes/users.js
@@ -4,6 +4,11 @@ const User = require("../models/user");
 
 usersRouter.post("/", async (req, res) => {
   const { username, name, password } = req.body;
+  if (!password || password.length < 4) {
+    return res
+      .status(400)
+      .json({ error: "Password required. Minimum 4 characters" });
+  }
   // hash the password using bcrypt and salt factor 10
   const passwordHash = await bcrypt.hash(password, 10);
   const newUser = new User({
--- todos-backend-auth/utils/middleware.js
+++ todos-backend-auth/utils/middleware.js
@@ -15,6 +15,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error: "invalid id" });
   } else if (error.name === "ValidationError") {
     return res.status(400).json({ error: error.message });
+  } else if (error.name === "MongoServerError") {
+    return res.status(400).json({ error: error.message });
   }
   next(error);
 };
